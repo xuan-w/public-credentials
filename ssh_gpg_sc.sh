fa4468bdaef76e0c0e8710a4fbff88245a2b8852
Xuan Wang <code@wangxuan.name>
2021-07-25 15:29:48 -0400

    add comment to script

--- ssh_gpg_sc.sh
+++ ssh_gpg_sc.sh
@@ -1,6 +1,9 @@
+
+#souce this file, don't execute it! or you don't have PATH set
 gpg --card-status
 gpg-connect-agent updatestartuptty /bye
 
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
+# normally shouldn't need this
 ssh-add
